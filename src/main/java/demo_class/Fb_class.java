86def46f7e8993b43c41412f9537cc69049b2b29
prana <prana@PK>
2022-10-19 17:54:19 +0530

    pushpak

--- src/main/java/demo_class/Fb_class.java
+++ src/main/java/demo_class/Fb_class.java
@@ -5,10 +5,10 @@ public class Fb_class {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("hiiiiii");
-		System.out.println("hello all");
+		
 		System.out.println("kundan");
 		System.out.println("krishna");
-		
+		System.out.println("pushpak");
 	
 	}
 
