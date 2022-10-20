2a533c81f3a44a48136dfdd40755b2970bd222a4
HP <HP@DESKTOP-PR4R1OC>
2022-10-20 15:12:47 +0530

    kw

--- src/main/java/demo_class/Fb_class.java
+++ src/main/java/demo_class/Fb_class.java
@@ -3,19 +3,6 @@ package demo_class;
 public class Fb_class {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-<<<<<<< HEAD
-		System.out.println("hiiiiii");
-		System.out.println("pravesh");
-		
-=======
-		System.out.println("hiiiiii");	
-	
-		System.out.println("krishna");
-		System.out.println("chetan");
-		
-	
->>>>>>> branch 'master' of https://github.com/Pravesh1911/Testing_Fb_validation.git
 	}
 
 }
